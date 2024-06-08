34536660e7ec52c3bf8834311c90cd3203e98415
Khang Tran <khang07087@gmail.com>
2024-06-08 00:01:41 -0400

    Add style to index file

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -3,12 +3,14 @@
     <x-cart></x-cart>
     <x-navbar :categories="$categories"></x-navbar>
     <main>
-        <div class="flex flex-wrap w-full gap-1 px-1 mt-6">
-            @unless(count($listings) == 0)
-            @foreach ($listings as $listing)
-                <x-card.listing-card :listing="$listing"/>
+        <div class="w-full mt-6">
+            <div class="flex flex-wrap gap-1 justify-center">
+                @unless(count($listings) == 0)
+                @foreach ($listings as $listing)
+                    <x-card.listing-card :listing="$listing"/>
 
-            @endforeach
+                @endforeach
+            </div>
         </div>
         {{-- </div> --}}
         @else
